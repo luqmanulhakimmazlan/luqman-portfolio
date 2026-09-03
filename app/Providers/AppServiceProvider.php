49935940587c86d6c8899a63519413911d7ba05e
luqman <149998463+luqmanicious@users.noreply.github.com>
2026-09-03 17:10:30 +0800

    Force HTTPS for assets in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tell Laravel to always use HTTPS when generating asset links in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
